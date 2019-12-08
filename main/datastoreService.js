ac5f00bd829f572a4b9d9c9f2a2da5b123695a7e
Potat0x <Potat0x@users.noreply.github.com>
2019-12-08 14:13:32 +0100

    Init

---
+++ main/datastoreService.js
@@ -0,0 +1,13 @@
+const url = "http://127.0.0.1:8080/datastore"
+
+function createDatastore(datastoreRequest, onSuccess, onError) {
+    console.log("create datastore 2")
+}
+
+function getDatastore(datastoreRequest) {
+    console.log("asd")
+}
+
+function deleteDatastore(datastoreRequest) {
+
+}
---
+++ main/main.js
@@ -0,0 +1,21 @@
+function createDatastoreOnclick() {
+  console.log("create datastore")
+  createDatastore("xd")
+}
+
+function deleteDatastoreOnclick() {
+  console.log("delete datastore")
+}
+
+function createAppOnclick() {
+  console.log("create app")
+  deleteAppOnclick()
+}
+
+function redeployAppOnclick() {
+  console.log("redeploy app")
+}
+
+function deleteAppOnclick() {
+  console.log("delete app")
+}
