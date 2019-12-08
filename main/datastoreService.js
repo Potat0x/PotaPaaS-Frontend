c65ddae8b3f281ec86984f28c46a5f71a957bcd4
Potat0x <Potat0x@users.noreply.github.com>
2019-12-08 19:32:50 +0100

    Creating datastores

--- main/datastoreService.js
+++ main/datastoreService.js
@@ -1,13 +1,53 @@
-const url = "http://127.0.0.1:8080/datastore"
-
-function createDatastore(datastoreRequest, onSuccess, onError) {
-    console.log("create datastore 2")
+const url = "http://localhost:8080/datastore"
+const authToken = ""
+function createDatastoreRequest(createDatastoreRequest, onSuccess, onError) {
+    console.log("datastoreService: create datastore: " + JSON.stringify(createDatastoreRequest))
+    return postRequest(JSON.stringify(createDatastoreRequest), onSuccess, onError)
 }
 
 function getDatastore(datastoreRequest) {
-    console.log("asd")
 }
 
 function deleteDatastore(datastoreRequest) {
+}
 
+function createDatastoreErrorMessage(errorJson) {
+    let message = "Error: " + errorJson.message
+    let validRequestExample = "<br>Valid request example:<br>"
+        + "name: " + errorJson.validRequestDtoExample.name + "<br>"
+        + "type: " + errorJson.validRequestDtoExample.type
+    return message + validRequestExample
+}
+
+function postRequest(requestBody, onSuccess, onError) {
+    return fetch(url, {
+        method: "post",
+        body: requestBody,
+        mode: 'cors',
+        headers: new Headers({
+            "Authorization": authToken,
+            "Content-Type": "application/json"
+        })
+    }).then(response => {
+        console.log("fetch response: " + response.status + JSON.stringify(response))
+        if (response.status === 200 || response.status === 201) {
+            return response.json()
+        } else if (response.status === 401 || response.status === 403) {
+            return Promise.reject("Auth error")
+        } else if (response.status >= 400 && response.status < 500) {
+            return response.json()
+                .then(errorJson =>
+                    Promise.reject(createDatastoreErrorMessage(errorJson))
+                )
+        } else {
+            return Promise.reject("Server error: " + response.status)
+        }
+    }).then(json => {
+        console.log("request ok: " + JSON.stringify(json))
+        onSuccess(json)
+        return json
+    }).catch(err => {
+        console.error("request error:" + err)
+        onError(err)
+    })
 }
--- main/main.js
+++ main/main.js
@@ -1,6 +1,71 @@
+function initDatastoreModal() {
+  clearDatastoreModalMessage()
+  setDatastoreModalFooterButtonsDisabled(false)
+  setDatastoreModalSpinnerVisible(false)
+  console.log("INIT DATASTORE")
+}
+
 function createDatastoreOnclick() {
   console.log("create datastore")
-  createDatastore("xd")
+
+  setDatastoreModalSpinnerVisible(true)
+  clearDatastoreModalMessage()
+  setDatastoreModalFooterButtonsDisabled(true)
+
+  createDatastoreRequest(createDatastoreRequestBody(), showSuccessMessage, showErrorMessage)
+}
+
+function createDatastoreRequestBody() {
+  return {
+    name: document.getElementById("datastore-name").value,
+    type: document.getElementById("datastore-type").value
+  }
+}
+
+function showSuccessMessage(json) {
+  console.log("showSuccessMessage: " + json)
+  setDatastoreModalMessage("Datastore created!", "success")
+  setDatastoreModalSpinnerVisible(false)
+  setTimeout(() => $('#newDatastoreModal').modal('hide'), 2000)
+}
+
+function showErrorMessage(message) {
+  console.log("onerrormsg")
+  setDatastoreModalMessage(message, "error")
+  setDatastoreModalSpinnerVisible(false)
+  setDatastoreModalFooterButtonsDisabled(false)
+}
+
+function setDatastoreModalFooterButtonsDisabled(disabled) {
+  const footer = document.getElementById("datastoreModalFooter")
+  const buttons = footer.getElementsByTagName("button")
+  for (let button of buttons) {
+    if (disabled) {
+      button.setAttribute("disabled", "disabled")
+    } else {
+      button.removeAttribute("disabled")
+    }
+  }
+}
+
+function setDatastoreModalMessage(message, style) {
+  const datastoreModalMessage = document.getElementById("datastoreModalMessage")
+  if (style === "success") {
+    datastoreModalMessage.style.color = "lime"
+  } else if (style === "error") {
+    datastoreModalMessage.style.color = "red"
+  } else {
+    datastoreModalMessage.style.color = "black"
+  }
+  datastoreModalMessage.innerHTML = message
+}
+
+function clearDatastoreModalMessage() {
+  setDatastoreModalMessage("")
+}
+
+function setDatastoreModalSpinnerVisible(visible) {
+  document.getElementById("createDatastoreModalSpinner").style.display = visible ? "block" : "none"
 }
 
 function deleteDatastoreOnclick() {
